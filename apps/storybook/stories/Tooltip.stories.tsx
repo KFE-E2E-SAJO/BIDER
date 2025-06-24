f189b5a717d42b59b64152cb575c6a76d31f92a8
choiyoungae <choiaeyoung1001@gmail.com>
2025-06-24 23:36:50 +0900

    feat: Tooltip 공통 컴포넌트 구현 (#3)

---
+++ apps/storybook/stories/Tooltip.stories.tsx
@@ -0,0 +1,33 @@
+import type { Meta, StoryObj } from '@storybook/react-vite';
+import {
+  Tooltip,
+  TooltipContent,
+  TooltipProvider,
+  TooltipTrigger,
+} from '@repo/ui/components/Tooltip/Tooltip';
+
+const meta: Meta<typeof Tooltip> = {
+  title: 'Components/Tooltip',
+  component: Tooltip,
+  tags: ['autodocs'],
+  parameters: {
+    layout: 'centered',
+  },
+};
+
+export default meta;
+
+type Story = StoryObj<typeof Tooltip>;
+
+export const Default: Story = {
+  render: () => (
+    <TooltipProvider>
+      <Tooltip>
+        <TooltipTrigger asChild>
+          <h3>버튼</h3>
+        </TooltipTrigger>
+        <TooltipContent>툴팁 내용입니다</TooltipContent>
+      </Tooltip>
+    </TooltipProvider>
+  ),
+};
