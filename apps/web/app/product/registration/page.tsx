00b56869daa4ff078e43f29eb59e306e6b70e6fb
choiyoungae <choiaeyoung1001@gmail.com>
2025-06-26 22:22:58 +0900

    feat: 상품 출품 기능 - 이미지 업로드 부분 구현 (#16)

---
+++ apps/web/app/product/registration/page.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import ImageUploadPreview from './ImageUploadPreview';
+
+const ProductRegistrationPage = () => {
+  return (
+    <div className="pt-10">
+      {/* 사진 업로드 */}
+      <ImageUploadPreview />
+    </div>
+  );
+};
+
+export default ProductRegistrationPage;
