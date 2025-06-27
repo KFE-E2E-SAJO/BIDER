a61e0e33b911d5e84be4fbfe52ff3a6dd317b318
choiyoungae <choiaeyoung1001@gmail.com>
2025-06-28 06:33:44 +0900

    feat: 상품 상세페이지 하단 입찰 바 기능 구현

--- apps/web/app/product/[shortId]/page.tsx
+++ apps/web/app/product/[shortId]/page.tsx
@@ -6,6 +6,7 @@ import { AlarmClock, PencilLine } from 'lucide-react';
 import { formatNumberWithComma } from '@/shared/lib/formatNumberWithComma';
 import { formatTimestamptz } from '@/shared/lib/formatTimestamp';
 import { Avatar } from '@repo/ui/components/Avatar/Avatar';
+import BottomBar from './BottomBar';
 
 const ProductDetailPage = async ({ params }: { params: { shortId: string } }) => {
   // test용 shorId  = uRMUuxqfyznoNTZhFmvhx4
@@ -46,46 +47,45 @@ const ProductDetailPage = async ({ params }: { params: { shortId: string } }) =>
   };
 
   return (
-    <main>
-      <div className="flex flex-col gap-[25px]">
-        {/* 이미지 슬라이더 */}
-        <ProductImageSlider images={mapped.images} />
-        {/* 경매 상품 내용 */}
-        <div className="typo-subtitle-bold">{mapped.productTitle}</div>
-        <div>
-          <div className="typo-caption-regular text-neutral-600">최고 입찰가</div>
-          <div className="typo-subtitle-bold">{'20,000원'}</div>
+    <div className="flex flex-col gap-[25px]">
+      {/* 이미지 슬라이더 */}
+      <ProductImageSlider images={mapped.images} />
+      {/* 경매 상품 내용 */}
+      <div className="typo-subtitle-bold">{mapped.productTitle}</div>
+      <div>
+        <div className="typo-caption-regular text-neutral-600">최고 입찰가</div>
+        <div className="typo-subtitle-bold">{'20,000원'}</div>
+      </div>
+      <div className="bg-neutral-050 flex w-full items-center justify-between px-[12px] py-[9px]">
+        <div className="flex items-center gap-[10px]">
+          <div className="flex items-center gap-[3px]">
+            <PencilLine size={12} className="text-neutral-600" />
+            <div className="text-[10px] text-neutral-600">입찰 시작가</div>
+          </div>
+          <div className="typo-caption-medium">{formatNumberWithComma(mapped.minPrice)}원</div>
         </div>
-        <div className="bg-neutral-050 flex w-full items-center justify-between px-[12px] py-[9px]">
-          <div className="flex items-center gap-[10px]">
-            <div className="flex items-center gap-[3px]">
-              <PencilLine size={12} className="text-neutral-600" />
-              <div className="text-[10px] text-neutral-600">입찰 시작가</div>
-            </div>
-            <div className="typo-caption-medium">{formatNumberWithComma(mapped.minPrice)}원</div>
+        <div className="h-[12px] w-[1px] bg-neutral-300"></div>
+        <div className="flex items-center gap-[10px]">
+          <div className="flex items-center gap-[3px]">
+            <AlarmClock size={12} className="text-neutral-600" />
+            <div className="text-[10px] text-neutral-600">입찰 마감 일자</div>
           </div>
-          <div className="h-[12px] w-[1px] bg-neutral-300"></div>
-          <div className="flex items-center gap-[10px]">
-            <div className="flex items-center gap-[3px]">
-              <AlarmClock size={12} className="text-neutral-600" />
-              <div className="text-[10px] text-neutral-600">입찰 마감 일자</div>
-            </div>
-            <div className="typo-caption-medium">{formatTimestamptz(mapped.auctionEndAt)}</div>
-          </div>
-        </div>
-        <div className="typo-body-regular">{mapped.productDescription}</div>
-        <div className="h-[8px] w-full bg-neutral-100"></div>
-        {/* 입찰현황판 */}
-        {/* <div className="h-[8px] w-full bg-neutral-100"></div> */}
-        <div className="flex items-center gap-[19px]">
-          <Avatar className="size-[36px]" />
-          <div className="flex flex-col gap-[5px]">
-            <div className="typo-body-medium">{mapped.exhibitUser.nickname}</div>
-            <div className="typo-caption-regular">{mapped.exhibitUser.address}</div>
-          </div>
+          <div className="typo-caption-medium">{formatTimestamptz(mapped.auctionEndAt)}</div>
         </div>
       </div>
-    </main>
+      <div className="typo-body-regular">{mapped.productDescription}</div>
+      <div className="h-[8px] w-full bg-neutral-100"></div>
+      {/* 입찰현황판 */}
+      {/* <div className="h-[8px] w-full bg-neutral-100"></div> */}
+      <div className="flex items-center gap-[19px]">
+        <Avatar className="size-[36px]" />
+        <div className="flex flex-col gap-[5px]">
+          <div className="typo-body-medium">{mapped.exhibitUser.nickname}</div>
+          <div className="typo-caption-regular">{mapped.exhibitUser.address}</div>
+        </div>
+      </div>
+      <BottomBar auctionEndAt={mapped.auctionEndAt} />
+    </div>
   );
 };
 
