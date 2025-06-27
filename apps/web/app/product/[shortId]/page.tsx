532ec01a47224e8b36e4cca8eafe5526cd1d114b
choiyoungae <choiaeyoung1001@gmail.com>
2025-06-28 05:08:40 +0900

    feat: auction 테이블과 연결 및 url에 전체 uuid가 뜨지 않도록 short-uuid 적용하여 화면 세팅

---
+++ apps/web/app/product/[shortId]/page.tsx
@@ -0,0 +1,46 @@
+import { decodeShortId } from '@/shared/lib/shortUuid';
+import { supabase } from '@/shared/lib/supabaseClient';
+import { notFound } from 'next/navigation';
+
+const ProductDetailPage = async ({ params }: { params: { shortId: string } }) => {
+  // test용 shorId  = uRMUuxqfyznoNTZhFmvhx4
+  const param = await params;
+  const uuid = decodeShortId(param.shortId);
+
+  const { data, error } = await supabase
+    .from('auction')
+    .select(
+      `
+            *,
+            product (
+            *,
+            exhibit_user:exhibit_user_id (
+                *
+            ),
+            product_image (
+                *
+            )
+            )
+        `
+    )
+    .eq('auction_id', uuid)
+    .single();
+
+  if (error || !data) return notFound();
+
+  const auction = data;
+  const mapped = {
+    auctionId: auction.auction_id,
+    productTitle: auction.product?.title,
+    productDescription: auction.product?.description,
+    sellerName: auction.product?.exhibit_user?.username,
+    images: auction.product?.product_image ?? [],
+    minPrice: auction.min_price,
+  };
+
+  console.log(data);
+
+  return <main>{JSON.stringify(data)}</main>;
+};
+
+export default ProductDetailPage;
