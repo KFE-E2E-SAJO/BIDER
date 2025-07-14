7f2ad0a1d3375f8ec11b90399a4bd9d544d1fd10
choiyoungae <choiaeyoung1001@gmail.com>
2025-07-14 21:00:11 +0900

    feat: 입찰 현황판에 realtime 적용하여 입찰 내역을 실시간으로 반영 (#158)

--- apps/web/features/auction/detail/ui/BiddingStatusBoard.tsx
+++ apps/web/features/auction/detail/ui/BiddingStatusBoard.tsx
@@ -1,16 +1,62 @@
 import { Crown } from 'lucide-react';
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { BiddingStatusBoardProps } from '../types';
 import { formatNumberWithComma } from '@/shared/lib/formatNumberWithComma';
+import { anonSupabase } from '@/shared/lib/supabaseClient';
+import { BidHistory, BidHistoryWithUserNickname } from '@/entities/bidHistory/model/types';
 
 const BiddingStatusBoard = ({ data }: BiddingStatusBoardProps) => {
   if (data.length === 0) {
     return <div className="text-center">아직 입찰자가 없습니다. 첫 입찰자가 되어보세요!</div>;
   }
 
+  const [bidData, setBidData] = useState<BidHistoryWithUserNickname[]>(data);
+
+  useEffect(() => {
+    const channel = anonSupabase
+      .channel('bid_history_changes')
+      .on(
+        'postgres_changes',
+        {
+          event: 'INSERT',
+          schema: 'public',
+          table: 'bid_history',
+          filter: `auction_id=eq.${data[0]?.auction_id}`,
+        },
+        async (payload) => {
+          const newBid = payload.new;
+          const { data: profiles, error } = await anonSupabase
+            .from('profiles')
+            .select('nickname')
+            .eq('user_id', newBid.bid_user_id)
+            .limit(1)
+            .single();
+
+          if (error) {
+            console.error('프로필 조회 실패:', error);
+            return;
+          }
+
+          const newBidWithNickname = {
+            ...(newBid as BidHistory),
+            bid_user_nickname: { nickname: profiles.nickname },
+          };
+
+          setBidData((prev) => [newBidWithNickname, ...prev]);
+        }
+      )
+      .subscribe((status) => {
+        console.log('SUBSCRIBE STATUS:', status);
+      });
+
+    return () => {
+      channel.unsubscribe();
+    };
+  }, []);
+
   return (
     <div>
-      {data.slice(0, 5).map((bid, index) => (
+      {bidData.slice(0, 5).map((bid, index) => (
         <div
           key={bid.bid_id}
           className={`flex justify-between border-b border-dashed border-neutral-300 px-[16px] py-[9px] ${
