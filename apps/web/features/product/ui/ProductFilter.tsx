ab700f985d3300b1d05b57ff2ad1974541c545fa
Haeun <haruyam15@gmail.com>
2025-07-15 20:24:44 +0900

    feat: 툴팁추가 (#145)

--- apps/web/features/product/ui/ProductFilter.tsx
+++ apps/web/features/product/ui/ProductFilter.tsx
@@ -1,6 +1,12 @@
 import { ProductFilter as ProductFilterType } from '@/features/product/types';
 import Checkbox from '@repo/ui/components/Checkbox/Checkbox';
 import { useEffect, useState } from 'react';
+import {
+  Tooltip,
+  TooltipContent,
+  TooltipProvider,
+  TooltipTrigger,
+} from '@repo/ui/components/Tooltip/Tooltip';
 
 interface ProductFilterProps {
   setFilter: (filters: ProductFilterType[]) => void;
@@ -26,13 +32,28 @@ const ProductFilter = ({ setFilter }: ProductFilterProps) => {
   }, [isDeadlineToday, isExcludeEnded]);
 
   return (
-    <div className="p-box flex gap-[17px] pb-5">
+    <div className="p-box relative flex gap-[17px] pb-5">
+      <div className="absolute">
+        <TooltipProvider>
+          <Tooltip>
+            <TooltipTrigger asChild>
+              <p className="opacity-0">툴팁</p>
+            </TooltipTrigger>
+            <TooltipContent side="bottom" className="mt-1">
+              오늘이 입찰 마지막 기회!
+            </TooltipContent>
+          </Tooltip>
+        </TooltipProvider>
+      </div>
+
       <Checkbox
         id="deadlineToday"
         label="오늘 마감"
         value="deadline-today"
         onCheckedChange={(checked) => handleCheckboxChange(checked === true, 'deadline-today')}
+        className="z-10"
       />
+
       <Checkbox
         id="excludeEnded"
         label="입찰 종료 제외"
