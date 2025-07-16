dd4cecc4fa1ff461c7230aa9871229f15e3742d0
Haeun <haruyam15@gmail.com>
2025-07-16 19:00:35 +0900

    feat: 지도로보기 토글(#145)

--- apps/web/app/(base)/page.tsx
+++ apps/web/app/(base)/page.tsx
@@ -10,10 +10,13 @@ import ProductListScroll from '@/features/product/ui/ProductListScroll';
 import ProductSortDropdown from '@/features/product/ui/ProductSortDropdown';
 import { useState } from 'react';
 import { ProductSort } from '@/features/product/types';
+import { Button } from '@repo/ui/components/Button/Button';
+import { List, Map } from 'lucide-react';
 
 const HomePage = () => {
   const userId = useAuthStore((state) => state.user?.id) as string;
   const [sort, setSort] = useState<ProductSort>('latest');
+  const [showMap, setShowMap] = useState(false); // ✅ 지도 보기 토글 상태
   const { data, isLoading, isError, error, fetchNextPage, hasNextPage, isFetchingNextPage } =
     useProductList({ userId, sort });
   useProductListErrorHandler(isError, error);
@@ -30,13 +33,14 @@ const HomePage = () => {
   if (isLoading) return <Loading />;
   return (
     <>
+      {showMap && <div className="h-[300px]">{/* <GoogleMap mapId="productList" /> */}</div>}
       <div className="p-box my-[21px] flex items-center justify-between">
         <LocationPin />
         <ProductSortDropdown setSort={setSort} />
       </div>
       <div
         ref={parentRef}
-        style={{ height: 'calc(100vh - 235px)' }}
+        style={{ height: showMap ? 'calc(100vh - 535px)' : 'calc(100vh - 235px)' }}
         className="p-box overflow-auto"
       >
         <ProductListScroll
@@ -46,6 +50,22 @@ const HomePage = () => {
           isFetchingNextPage={isFetchingNextPage}
         />
       </div>
+      <Button
+        shape="rounded"
+        size="fit"
+        className="bottom-30 text-caption fixed left-1/2 -translate-x-1/2 bg-neutral-900"
+        onClick={() => setShowMap((prev) => !prev)}
+      >
+        {showMap ? (
+          <>
+            <List size="20" /> 리스트로 보기
+          </>
+        ) : (
+          <>
+            <Map size="20" /> 지도로 보기
+          </>
+        )}
+      </Button>
     </>
   );
 };
