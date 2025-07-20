247275ee4558dcdbf3e07c1f4f97e8d5beda3d72
Haeun <haruyam15@gmail.com>
2025-07-21 03:24:04 +0900

    chore: 스플래시 이미지추가(#199)

--- apps/web/app/layout.tsx
+++ apps/web/app/layout.tsx
@@ -38,54 +38,59 @@ const RootLayout = ({
           href="/ios/apple-splash-640x1136.png"
           media="(device-width: 320px) and (device-height: 568px) and (-webkit-device-pixel-ratio: 2)"
         />
-
         {/* iPhone 8  */}
         <link
           rel="apple-touch-startup-image"
           href="/ios/apple-splash-750x1334.png"
           media="(device-width: 375px) and (device-height: 667px) and (-webkit-device-pixel-ratio: 2)"
         />
-
         {/* iPhone 8 Plus  */}
         <link
           rel="apple-touch-startup-image"
           href="/ios/apple-splash-1242x2208.png"
           media="(device-width: 414px) and (device-height: 736px) and (-webkit-device-pixel-ratio: 3)"
         />
-
         {/* iPhone 11  */}
         <link
           rel="apple-touch-startup-image"
           href="/ios/apple-splash-828x1792.png"
           media="(device-width: 414px) and (device-height: 896px) and (-webkit-device-pixel-ratio: 2)"
         />
-
         {/* iPhone X, 11 Pro  */}
         <link
           rel="apple-touch-startup-image"
           href="/ios/apple-splash-1125x2436.png"
           media="(device-width: 375px) and (device-height: 812px) and (-webkit-device-pixel-ratio: 3)"
         />
-
         {/* iPhone 12, 13, 14  */}
         <link
           rel="apple-touch-startup-image"
           href="/ios/apple-splash-1170x2532.png"
           media="(device-width: 390px) and (device-height: 844px) and (-webkit-device-pixel-ratio: 3)"
         />
-
-        {/* iPhone 14 Pro  */}
+        {/* iPhone 14,15 Pro  */}
         <link
           rel="apple-touch-startup-image"
           href="/ios/apple-splash-1179x2556.png"
           media="(device-width: 393px) and (device-height: 852px) and (-webkit-device-pixel-ratio: 3)"
         />
-
-        {/* iPhone 14 Pro Max  */}
+        {/* iPhone 14,15 Pro Max  */}
         <link
           rel="apple-touch-startup-image"
           href="/ios/apple-splash-1290x2796.png"
           media="(device-width: 430px) and (device-height: 932px) and (-webkit-device-pixel-ratio: 3)"
+        />
+        {/* iPhone 12 mini */}
+        <link
+          rel="apple-touch-startup-image"
+          href="/ios/apple-splash-1080x2340.png"
+          media="(device-width: 360px) and (device-height: 780px) and (-webkit-device-pixel-ratio: 3)"
+        />
+        {/* iPhone XS Max / 11 Pro Max */}
+        <link
+          rel="apple-touch-startup-image"
+          href="/ios/apple-splash-1242x2688.png"
+          media="(device-width: 414px) and (device-height: 896px) and (-webkit-device-pixel-ratio: 3)"
         />
       </head>
       <body>
