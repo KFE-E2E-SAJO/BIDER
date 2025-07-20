411dee09f93944173f4d0ed8a0ff9579148a61fa
Haeun <haruyam15@gmail.com>
2025-07-21 01:29:02 +0900

    chore: PWA셋팅, push 테스트(#199)

--- apps/web/app/layout.tsx
+++ apps/web/app/layout.tsx
@@ -1,23 +1,19 @@
 import type { Metadata } from 'next';
-import localFont from 'next/font/local';
 import type { Viewport } from 'next';
 import '@repo/ui/styles.css';
 import '../styles/global.css';
 import ReactQueryProvider from '@/shared/providers/ReactQueryProvider';
 import { Toaster } from '@repo/ui/components/Toast/Sonner';
 
-const geistSans = localFont({
-  src: '../public/fonts/GeistVF.woff',
-  variable: '--font-geist-sans',
-});
-const geistMono = localFont({
-  src: '../public/fonts/GeistMonoVF.woff',
-  variable: '--font-geist-mono',
-});
-
 export const metadata: Metadata = {
   title: '가장 가까운 경매장 | Bider',
   description: '내 근처 가장 가까운 경매장, Bider',
+  manifest: '/manifest.webmanifest',
+  appleWebApp: {
+    capable: true,
+    title: 'Bider',
+    statusBarStyle: 'default',
+  },
 };
 
 export const viewport: Viewport = {
@@ -34,7 +30,7 @@ const RootLayout = ({
 }>) => {
   return (
     <html lang="ko">
-      <body className={`${geistSans.variable} ${geistMono.variable}`}>
+      <body>
         <ReactQueryProvider>
           <div id="container" className="flex min-h-screen flex-col">
             {/* 'pb-헤더높이'로 수정 */}
