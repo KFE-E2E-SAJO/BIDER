3781e24b3a4315cf3d53b8e6a168eab62ddcaab4
Haeun <haruyam15@gmail.com>
2025-07-21 03:51:58 +0900

    chore:ios 스플래시 적용(#199)

--- apps/web/app/layout.tsx
+++ apps/web/app/layout.tsx
@@ -9,10 +9,71 @@ export const metadata: Metadata = {
   title: '가장 가까운 경매장 | Bider',
   description: '내 근처 가장 가까운 경매장, Bider',
   manifest: '/manifest.webmanifest',
+  icons: {
+    icon: '/favicon.ico',
+    shortcut: '/ios/apple-touch-icon.png',
+    apple: '/ios/apple-touch-icon.png',
+    other: {
+      rel: 'apple-touch-icon-precomposed',
+      url: '/ios/apple-touch-icon.png',
+    },
+  },
   appleWebApp: {
     capable: true,
     title: 'Bider',
     statusBarStyle: 'default',
+    startupImage: [
+      {
+        url: '/ios/apple-splash-640x1136.png',
+        media:
+          '(device-width: 320px) and (device-height: 568px) and (-webkit-device-pixel-ratio: 2)',
+      },
+      {
+        url: '/ios/apple-splash-750x1334.png',
+        media:
+          '(device-width: 375px) and (device-height: 667px) and (-webkit-device-pixel-ratio: 2)',
+      },
+      {
+        url: '/ios/apple-splash-1242x2208.png',
+        media:
+          '(device-width: 414px) and (device-height: 736px) and (-webkit-device-pixel-ratio: 3)',
+      },
+      {
+        url: '/ios/apple-splash-828x1792.png',
+        media:
+          '(device-width: 414px) and (device-height: 896px) and (-webkit-device-pixel-ratio: 2)',
+      },
+      {
+        url: '/ios/apple-splash-1125x2436.png',
+        media:
+          '(device-width: 375px) and (device-height: 812px) and (-webkit-device-pixel-ratio: 3)',
+      },
+      {
+        url: '/ios/apple-splash-1170x2532.png',
+        media:
+          '(device-width: 390px) and (device-height: 844px) and (-webkit-device-pixel-ratio: 3)',
+      },
+      {
+        url: '/ios/apple-splash-1179x2556.png',
+        media:
+          '(device-width: 393px) and (device-height: 852px) and (-webkit-device-pixel-ratio: 3)',
+      },
+      {
+        url: '/ios/apple-splash-1290x2796.png',
+        media:
+          '(device-width: 430px) and (device-height: 932px) and (-webkit-device-pixel-ratio: 3)',
+      },
+      {
+        url: '/ios/apple-splash-1080x2340.png',
+        media:
+          '(device-width: 360px) and (device-height: 780px) and (-webkit-device-pixel-ratio: 3)',
+      },
+      {
+        url: '/ios/apple-splash-1242x2688.png',
+        media:
+          '(device-width: 414px) and (device-height: 896px) and (-webkit-device-pixel-ratio: 3)',
+      },
+    ],
   },
 };
 
@@ -30,69 +91,6 @@ const RootLayout = ({
 }>) => {
   return (
     <html lang="ko">
-      <head>
-        <link rel="apple-touch-icon" href="/ios/apple-touch-icon.png" />
-        {/* iPhone SE  */}
-        <link
-          rel="apple-touch-startup-image"
-          href="/ios/apple-splash-640x1136.png"
-          media="(device-width: 320px) and (device-height: 568px) and (-webkit-device-pixel-ratio: 2)"
-        />
-        {/* iPhone 8  */}
-        <link
-          rel="apple-touch-startup-image"
-          href="/ios/apple-splash-750x1334.png"
-          media="(device-width: 375px) and (device-height: 667px) and (-webkit-device-pixel-ratio: 2)"
-        />
-        {/* iPhone 8 Plus  */}
-        <link
-          rel="apple-touch-startup-image"
-          href="/ios/apple-splash-1242x2208.png"
-          media="(device-width: 414px) and (device-height: 736px) and (-webkit-device-pixel-ratio: 3)"
-        />
-        {/* iPhone 11  */}
-        <link
-          rel="apple-touch-startup-image"
-          href="/ios/apple-splash-828x1792.png"
-          media="(device-width: 414px) and (device-height: 896px) and (-webkit-device-pixel-ratio: 2)"
-        />
-        {/* iPhone X, 11 Pro  */}
-        <link
-          rel="apple-touch-startup-image"
-          href="/ios/apple-splash-1125x2436.png"
-          media="(device-width: 375px) and (device-height: 812px) and (-webkit-device-pixel-ratio: 3)"
-        />
-        {/* iPhone 12, 13, 14  */}
-        <link
-          rel="apple-touch-startup-image"
-          href="/ios/apple-splash-1170x2532.png"
-          media="(device-width: 390px) and (device-height: 844px) and (-webkit-device-pixel-ratio: 3)"
-        />
-        {/* iPhone 14,15 Pro  */}
-        <link
-          rel="apple-touch-startup-image"
-          href="/ios/apple-splash-1179x2556.png"
-          media="(device-width: 393px) and (device-height: 852px) and (-webkit-device-pixel-ratio: 3)"
-        />
-        {/* iPhone 14,15 Pro Max  */}
-        <link
-          rel="apple-touch-startup-image"
-          href="/ios/apple-splash-1290x2796.png"
-          media="(device-width: 430px) and (device-height: 932px) and (-webkit-device-pixel-ratio: 3)"
-        />
-        {/* iPhone 12 mini */}
-        <link
-          rel="apple-touch-startup-image"
-          href="/ios/apple-splash-1080x2340.png"
-          media="(device-width: 360px) and (device-height: 780px) and (-webkit-device-pixel-ratio: 3)"
-        />
-        {/* iPhone XS Max / 11 Pro Max */}
-        <link
-          rel="apple-touch-startup-image"
-          href="/ios/apple-splash-1242x2688.png"
-          media="(device-width: 414px) and (device-height: 896px) and (-webkit-device-pixel-ratio: 3)"
-        />
-      </head>
       <body>
         <ReactQueryProvider>
           <div id="container" className="flex min-h-screen flex-col">
