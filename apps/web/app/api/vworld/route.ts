86e15f13156258bda4750ffe247fde1f9edb7fe5
Haeun <haruyam15@gmail.com>
2025-07-24 19:22:28 +0900

    hotfix: vworld 키 제거 (#262)

--- apps/web/app/api/vworld/route.ts
+++ apps/web/app/api/vworld/route.ts
@@ -1,10 +1,12 @@
 import { NextRequest, NextResponse } from 'next/server';
 
+const KEY = process.env.NEXT_PUBLIC_VWORLD_KEY;
+
 export async function GET(req: NextRequest) {
   const searchParams = req.nextUrl.searchParams;
   const point = searchParams.get('point');
 
-  const url = `https://api.vworld.kr/req/address?service=address&request=getAddress&type=both&crs=epsg:4326&zipcode=false&simple=false&format=json&key=2DBB9BE5-D790-349B-A105-250C70BCAFD3&point=${point}`;
+  const url = `https://api.vworld.kr/req/address?service=address&request=getAddress&type=both&crs=epsg:4326&zipcode=false&simple=false&format=json&key=${KEY}&point=${point}`;
 
   const res = await fetch(url);
   const data = await res.json();
