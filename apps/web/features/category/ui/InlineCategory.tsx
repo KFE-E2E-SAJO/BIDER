227442fa0afed7b553365ae1ed3921105925ac48
minok <minok9708@gmail.com>
2025-07-31 05:56:57 +0900

    refactor: category image <picture>사용하여 성능 개선(#330)

--- apps/web/features/category/ui/InlineCategory.tsx
+++ apps/web/features/category/ui/InlineCategory.tsx
@@ -26,7 +26,10 @@ const InlineCategory = ({ onSelect }: InlineCategoryProps) => {
                 onClick={() => onSelect(item.value)}
                 className="flex flex-col items-center gap-2"
               >
-                <Image src={item.src} alt={item.label} width={42} height={42} />
+                <picture>
+                  <source srcSet={item.src.replace('.png', '.webp')} type="image/webp" />
+                  <Image src={item.src} alt={item.label} width={42} height={42} />
+                </picture>
               </button>
             </div>
             <span className="typo-body-regular whitespace-nowrap text-center text-neutral-900">
