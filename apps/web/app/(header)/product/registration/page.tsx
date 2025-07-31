6183f864b698f3d32d970ccac078cea101d40bc9
Haeun <haruyam15@gmail.com>
2025-07-31 23:02:06 +0900

    refactor:QueryProvider위치 변경, 지도 컴포넌트 SSR 시 정적 이미지로 LCP 및 CLS 개선(#333)

--- apps/web/app/(header)/product/registration/page.tsx
+++ apps/web/app/(header)/product/registration/page.tsx
@@ -1,11 +1,14 @@
 import React from 'react';
 import { ProductRegistrationForm } from '@/features/product/ui/ProductRegistrationForm';
+import ReactQueryProvider from '@/shared/providers/ReactQueryProvider';
 
 const ProductRegistrationPage = () => {
   return (
-    <div className="pt-[16px]">
-      <ProductRegistrationForm />
-    </div>
+    <ReactQueryProvider>
+      <div className="pt-[16px]">
+        <ProductRegistrationForm />
+      </div>
+    </ReactQueryProvider>
   );
 };
 
