43fa34f888381178e61998090f667098b8b5333f
ksy1128 <ksy1128@echomarketing.co.kr>
2025-08-05 23:25:34 +0900

    hotfix: 제안하기 userid 방식 변경(#409)

--- apps/web/app/api/auction/[shortId]/proposal/target-product/route.ts
+++ apps/web/app/api/auction/[shortId]/proposal/target-product/route.ts
@@ -1,4 +1,3 @@
-import getUserId from '@/shared/lib/getUserId';
 import { supabase } from '@/shared/lib/supabaseClient';
 import { NextRequest, NextResponse } from 'next/server';
 import shortUUID from 'short-uuid';
@@ -9,7 +8,7 @@ export async function GET(request: NextRequest) {
   try {
     const { searchParams } = new URL(request.url);
     const shortId = searchParams.get('shortId');
-    const userId = await getUserId();
+    const userId = searchParams.get('userId');
 
     if (!userId || !shortId) {
       throw new Error('요청 정보가 부족합니다.');
